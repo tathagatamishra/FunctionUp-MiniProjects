99083e3e2571a21837f72c7d255ef2a814245582
tathagatamishra <tathagatamishra007@gmail.com>
2023-03-02 14:05:33 +0530

    server setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-const WebSocket = require('ws')
-const server = new WebSocket.Server({port:'6969'})
 
-server.on('connection', socket=>{
-    socket.on('message', message=>{
-        socket.send(message)
-    })
-})
+const http = require('http').createServer();
+
+const io = require('socket.io')(http, {
+    cors: { origin: "*" }
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('message', (message) =>     {
+        console.log(message);
+        io.emit('message', `${socket.id.substr(0,2)} said ${message}` );   
+    });
+});
+
+http.listen(8080, () => console.log('listening on http://localhost:8080') );
